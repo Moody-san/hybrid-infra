4f54887888222e060c665ae48c23ce0f0d63cac8
Moody-san <arshadhassan420@gmail.com>
2023-08-13 02:13:07 +0000

    commented out servers

--- oracle_resources.tf
+++ oracle_resources.tf
@@ -1,5 +1,5 @@
 data "oci_identity_availability_domains" "ads" {
-  compartment_id = var.compartment_id
+  compartment_id = var.oci_compartment_id
 }
 
 module "oraclenetwork" {
@@ -7,27 +7,27 @@ module "oraclenetwork" {
     oci = oci.oci_us
   }
   source         = "./Modules/Oracle_Module/network"
-  compartment_id = var.compartment_id
+  compartment_id = var.oci_compartment_id
   AD             = data.oci_identity_availability_domains.ads.availability_domains[2]["name"]
 }
 
-module "oracleservers" {
-  providers = {
-    oci = oci.oci_us
-  }
-  source         = "./Modules/Oracle_Module/compute"
-  for_each       = { for server in var.oracleservers : server.display_name => server }
-  cpu            = each.value.cpu
-  memory         = each.value.memory
-  AD             = data.oci_identity_availability_domains.ads.availability_domains[2]["name"] // for ad=3
-  server_name    = each.value.display_name
-  subnet_id      = module.oraclenetwork.subnet_id
-  image_id       = var.image_id
-  ssh_key        = var.ssh_key
-  compartment_id = var.compartment_id
-  depends_on     = [module.oraclenetwork]
-}
+# module "oracleservers" {
+#   providers = {
+#     oci = oci.oci_us
+#   }
+#   source         = "./Modules/Oracle_Module/compute"
+#   for_each       = { for server in local.oracleservers : server.display_name => server }
+#   cpu            = each.value.cpu
+#   memory         = each.value.memory
+#   AD             = data.oci_identity_availability_domains.ads.availability_domains[2]["name"] // for ad=3
+#   server_name    = each.value.display_name
+#   subnet_id      = module.oraclenetwork.subnet_id
+#   ssh_key        = var.ssh_key
+#   compartment_id = var.oci_compartment_id
+#   image_id       = each.value.image
+#   depends_on     = [module.oraclenetwork]
+# }
 
-output "oracle_server_public_ip" {
-  value = { for k, v in module.oracleservers : k => v.public_ip }
-}
+# output "oracle_server_public_ip" {
+#   value = { for k, v in module.oracleservers : k => v.public_ip }
+# }
