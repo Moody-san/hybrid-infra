e9d7a0c7a0899ee65f9d3d3b4ff8bffe753269bf
Moody-san <arshadhassan420@gmail.com>
2024-04-21 14:27:46 +0500

    added spot comment & remove unnecessary variables

--- variables.tf
+++ variables.tf
@@ -6,15 +6,7 @@ variable "oci_compartment_id" {
 
 }
 variable "oci_ubuntu22_id" {
-
-}
-
-variable "oci_ubuntu20_id" {
-
-}
-
-variable "oci_oraclelinux8_id" {
-
+    default = "ocid1.image.oc1.phx.aaaaaaaa327eqiyphdvv4imkbpjcrhsf543cfmx3bttxzfu7tmae4kk7kqua"
 }
 
 variable "az_subscription_id" {
